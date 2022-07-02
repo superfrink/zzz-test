c65f1c8651e6c9873b30ef67290593f8c9a3f771
Chad Clark <chad-git-p.superfrink@superfrink.net>
2022-07-01 21:22:39 -0600

    test

--- .github/workflows/test-data/script-verify-database-data.pl
+++ .github/workflows/test-data/script-verify-database-data.pl
@@ -11,50 +11,36 @@ my $dbh = DBI->connect($dsn, $user, $password);
 
 my $failed = 0;
 
-my $sth = $dbh->prepare(
-    'SELECT * from mytable WHERE foo = ?')
-    or die "prepare statement failed: $dbh->errstr()";
-$sth->execute('asdf') or die "execution failed: $dbh->errstr()";
-print $sth->rows . " rows found.\n";
-if (1 != $sth->rows) {
+sub look_for_row_foo($);
+
+my $row_count = look_for_row_foo('asdf');
+if (1 != $row_count) {
     $failed ++;
     print STDERR "'asdf' row not found\n";
 }
-while (my $ref = $sth->fetchrow_hashref()) {
-    print "Found row: foo = $ref->{'foo'}, bar = $ref->{'bar'}\n";
-}
-$sth->finish;
 
-$sth = $dbh->prepare(
-    'SELECT * from mytable WHERE foo = ?')
-    or die "prepare statement failed: $dbh->errstr()";
-$sth->execute('lmno') or die "execution failed: $dbh->errstr()";
-print $sth->rows . " rows found.\n";
-if (1 != $sth->rows) {
-    $failed ++;
-    print STDERR "'lmno' row not found\n";
-}
-while (my $ref = $sth->fetchrow_hashref()) {
-    print "Found row: foo = $ref->{'foo'}, bar = $ref->{'bar'}\n";
-}
-$sth->finish;
-
-$sth = $dbh->prepare(
-    'SELECT * from mytable WHERE foo = ?')
-    or die "prepare statement failed: $dbh->errstr()";
-$sth->execute('nosuchrow') or die "execution failed: $dbh->errstr()";
-print $sth->rows . " rows found.\n";
-if (0 != $sth->rows) {
-    $failed ++;
-    print STDERR "'nosuchrow' row found\n";
-}
-while (my $ref = $sth->fetchrow_hashref()) {
-    print "Found row: foo = $ref->{'foo'}, bar = $ref->{'bar'}\n";
-}
-$sth->finish;
 
 # goal: check for failed testw
 
 if (0 != $failed) {
     die "Tests failed: ", $failed, "\n";
+}
+
+
+sub look_for_row_foo() {
+
+    my $foo = shift;
+
+    print "looking for $foo\n";
+    my $sth = $dbh->prepare(
+        'SELECT * from mytable WHERE foo = ?')
+        or die "prepare statement failed: $dbh->errstr()";
+    $sth->execute($foo) or die "execution failed: $dbh->errstr()";
+    my $row_count = $sth->rows;
+    while (my $ref = $sth->fetchrow_hashref()) {
+        print "Found row: foo = $ref->{'foo'}, bar = $ref->{'bar'}\n";
+    }
+    $sth->finish;
+
+    return  $row_count;
 }
