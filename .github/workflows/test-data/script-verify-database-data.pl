2091355ac17cfc62cdb2ebe7fba64d88b26e095b
Chad Clark <chad-git-p.superfrink@superfrink.net>
2022-07-01 21:02:11 -0600

    test

---
+++ .github/workflows/test-data/script-verify-database-data.pl
@@ -0,0 +1,20 @@
+#! /usr/bin/perl -w
+
+use strict;
+use DBI;
+
+my $database = $ENV{'DB_DATABASE'};
+my $user = $ENV{'DB_USER'};
+my $password = $ENV{'DB_PASSWORDJ'};
+my $dsn = "DBI:mysql:database=$database;host=127.0.0.0;port=3306";
+my $dbh = DBI->connect($dsn, $user, $password);
+
+my $sth = $dbh->prepare(
+    'SELECT * from mytable WHERE foo = ?')
+    or die "prepare statement failed: $dbh->errstr()";
+$sth->execute() or die "execution failed: $dbh->errstr()";
+print $sth->rows . " rows found.\n";
+while (my $ref = $sth->fetchrow_hashref()) {
+    print "Found row: id = $ref->{'id'}, fn = $ref->{'first_name'}\n";
+}
+$sth->finish;
